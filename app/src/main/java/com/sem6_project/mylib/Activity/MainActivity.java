f1e00713907cc78f9669ad3764fe2c0f454f1e81
Devarsh Ukani <186620307056@darshan.ac.in>
2021-03-16 05:31:01 +0530

    Final Design final final 16/3

--- app/src/main/java/com/sem6_project/mylib/Activity/MainActivity.java
+++ app/src/main/java/com/sem6_project/mylib/Activity/MainActivity.java
@@ -23,8 +23,8 @@ import org.json.JSONObject;
 // pull test
 public class MainActivity<keeplogin> extends AppCompatActivity {
 
-    public final static String ip = "https://projectmylib.000webhostapp.com/";
-    //public final static String ip = "http://192.168.43.181/";
+    //public final static String ip = "https://projectmylib.000webhostapp.com/";
+    public final static String ip = "http://192.168.137.133/";
     Button btn_login;
     EditText et_id, et_pass;
     String json1 = ip + "mylib/json/user.php?id=";
